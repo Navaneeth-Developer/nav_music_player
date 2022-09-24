9032b4d862acc9fb720db82edc91d3a0767c2c86
Dev-Navaneeth <navaneeth9698.ait@gmail.com>
2022-09-24 16:53:29 +0530

    Slider worked

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -24,12 +24,12 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
-    setSongInfo({...songInfo,currentTime:current,duration:duration})
+    setSongInfo({ ...songInfo, currentTime: current, duration: duration });
   };
 
-  const getTime = (time)=>{
-    return(
-        Math.floor(time/60)+":"+("0"+Math.floor(time%60)).slice(-2)
+  const getTime = (time) => {
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
     );
   };
 
@@ -37,11 +37,16 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     currentTime: null,
     duration: null,
   });
+
+  const dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value
+    setSongInfo({...songInfo,currentTime:e.target.value})
+  };
   return (
     <div className="player">
       <div className="time-controller">
         <p>{getTime(songInfo.currentTime)}</p>
-        <input type="range" name="" id="" />
+        <input min={0} max={songInfo.duration} value={songInfo.currentTime} onChange={dragHandler} type="range" name="" id="" />
         <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-control">
@@ -54,7 +59,12 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
         />
         <FontAwesomeIcon className="skip-right" size="2x" icon={faAngleRight} />
       </div>
-      <audio onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
     </div>
   );
 };
