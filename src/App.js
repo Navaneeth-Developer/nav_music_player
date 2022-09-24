fc9d1a686c79612ac939c86ccebc14f13ed7ecb8
Dev-Navaneeth <navaneeth9698.ait@gmail.com>
2022-09-24 14:32:27 +0530

    data api was created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./styles/app.scss"
 import Song from "./components/Song";
 import Player from "./components/Player";
+import data from "./util"
 function App() {
   return (
     <div className="App">
-     <h1>Nav Music Player</h1>
      <Song/>
      <Player/>
     </div> 
